f9bf7a7367041395e3214d19b3be15bc9b5c7bff
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2017-07-02 22:29:37 +0300

    Prefabs and Animation

---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    private const float LERP_SPEED = 0.2f;
+    private Vector3 _targetPos;
+    private float _lastFall = 0;
+
+    void Start()
+    {
+        _targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - _lastFall >= 1)
+        {
+            _targetPos += new Vector3(0, -1, 0);
+            _lastFall = Time.time;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, _targetPos, LERP_SPEED);
+    }
+}
