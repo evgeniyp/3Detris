0fc73f00bcdec0ab53d2340512741f37f47cf2ae
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2017-07-03 23:50:08 +0300

    Rotation and movement through linked mirror game object

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -1,47 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Group : MonoBehaviour
 {
-    private const float LERP_SPEED = 0.2f;
+    private const float GAME_TICK_SECONDS = 1f;
+    private const float LERP_SPEED = 0.1f;
 
-    private Vector3 _targetPos;
     private Quaternion _targetRot;
 
     private float _lastFall = 0;
 
+    private GameObject _real;
+
     void Start()
     {
-        _targetPos = transform.position;
-        _targetRot = transform.rotation;
+        _real = new MirrorGameObject(gameObject).Me;
     }
 
     void Update()
     {
-        if (Time.time - _lastFall >= 5)
+        if (Time.time - _lastFall >= GAME_TICK_SECONDS)
         {
-            _targetPos += new Vector3(0, -1, 0);
+            _real.transform.position += new Vector3(0, -1, 0);
             _lastFall = Time.time;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            _targetRot *= Quaternion.Euler(Vector3.left * 90);
+            _real.transform.rotation *= Quaternion.Euler(Vector3.forward * 90);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            _targetRot *= Quaternion.Euler(Vector3.left * -90);
+            _real.transform.rotation *= Quaternion.Euler(Vector3.forward * -90);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            _targetRot *= Quaternion.Euler(Vector3.forward * -90);
+            _real.transform.rotation *= Quaternion.Euler(Vector3.right * -90);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            _targetRot *= Quaternion.Euler(Vector3.forward * 90);
+            _real.transform.rotation *= Quaternion.Euler(Vector3.right * 90);
         }
 
-        transform.position = Vector3.Lerp(transform.position, _targetPos, LERP_SPEED);
-        transform.rotation = Quaternion.Lerp(transform.rotation, _targetRot, LERP_SPEED);
+        transform.position = Vector3.Lerp(transform.position, _real.transform.position, LERP_SPEED);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _real.transform.rotation, LERP_SPEED);
     }
 }
---
+++ Assets/Scripts/MirrorGameObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MirrorGameObject
+{
+    public GameObject Me;
+    public GameObject Link;
+
+    public MirrorGameObject(GameObject link)
+    {
+        Link = link;
+        Me = new GameObject();
+
+        Me.transform.position = link.transform.position;
+        Me.transform.rotation = link.transform.rotation;
+
+        foreach (Transform linkChild in link.transform)
+        {
+            var child = new MirrorGameObject(linkChild.gameObject);
+            child.Me.transform.SetParent(Me.transform);
+            child.Me.transform.position = linkChild.position;
+            child.Me.transform.rotation = linkChild.rotation;
+        }
+    }
+}
