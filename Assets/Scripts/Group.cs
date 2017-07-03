04df1823edbdadc57f1046aa5d47ad568ab6b382
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2017-07-03 16:55:00 +0300

    Rotation prototype

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Group : MonoBehaviour
 {
     private const float LERP_SPEED = 0.2f;
+
     private Vector3 _targetPos;
+    private Quaternion _targetRot;
+
     private float _lastFall = 0;
 
     void Start()
@@ -15,12 +18,29 @@ public class Group : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - _lastFall >= 1)
+        if (Time.time - _lastFall >= 5)
         {
             _targetPos += new Vector3(0, -1, 0);
             _lastFall = Time.time;
         }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _targetRot.eulerAngles += new Vector3(0, -90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _targetRot.eulerAngles += new Vector3(0, +90, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _targetRot.eulerAngles += new Vector3(+90, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _targetRot.eulerAngles += new Vector3(-90, 0, 0);
+        }
 
         transform.position = Vector3.Lerp(transform.position, _targetPos, LERP_SPEED);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _targetRot, LERP_SPEED);
     }
 }
