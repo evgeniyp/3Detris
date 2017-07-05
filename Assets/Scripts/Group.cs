681021be36ffe5952e426b192236e5dd1159aeae
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2017-07-05 23:06:02 +0300

    Major fixes

--- Assets/Scripts/Group.cs
+++ Assets/Scripts/Group.cs
@@ -2,7 +2,7 @@
 
 public class Group : MonoBehaviour
 {
-    private const float GAME_TICK_SECONDS = 0.5f;
+    private const float GAME_TICK_SECONDS = 0.2f;
     private const float LERP_SPEED = 0.2f;
 
     private bool _drivenByPlayer = true;
@@ -13,29 +13,32 @@ public class Group : MonoBehaviour
 
     private void Start()
     {
-        var gameObject = new GameObject();
-        gameObject.transform.position = transform.position;
-        gameObject.transform.rotation = transform.rotation;
-        gameObject.AddComponent<GroupReal>();
-        _real = gameObject.GetComponent<GroupReal>();
+        var o = new GameObject();
+        o.transform.position = transform.position;
+        o.transform.rotation = transform.rotation;
+        o.AddComponent<GroupReal>();
+        _real = o.GetComponent<GroupReal>();
         _real.DisplayedGroup = this;
 
         foreach (Transform child in transform)
         {
-            var childGameObject = new GameObject();
-            childGameObject.transform.position = child.position;
-            childGameObject.transform.rotation = child.rotation;
-            childGameObject.transform.SetParent(_real.transform);
-            childGameObject.AddComponent<CubeReal>();
-            var _realChild = childGameObject.GetComponent<CubeReal>();
+            var c = new GameObject();
+            c.transform.position = child.position;
+            c.transform.rotation = child.rotation;
+            c.transform.SetParent(_real.transform);
+            c.AddComponent<CubeReal>();
+            var _realChild = c.GetComponent<CubeReal>();
             _realChild.DisplayedCube = child.GetComponent<Cube>();
         }
 
         if (!IsValidGridPos())
         {
             Debug.Log("GAME OVER");
+            Destroy(_real.gameObject);
             Destroy(gameObject);
         }
+        else
+            UpdateGrid();
     }
 
     bool IsValidGridPos()
@@ -47,9 +50,9 @@ public class Group : MonoBehaviour
             if (!Grid.InsideBorder(v))
                 return false;
 
-            if (Grid.grid[(int)v.x, (int)v.z, (int)v.y] != null &&
-                Grid.grid[(int)v.x, (int)v.z, (int)v.y].transform.parent != _real.transform)
-                return false;
+            if (Grid.grid[(int)v.x, (int)v.z, (int)v.y] != null)
+                if (Grid.grid[(int)v.x, (int)v.z, (int)v.y].transform.parent != _real.transform)
+                    return false;
         }
         return true;
     }
@@ -58,7 +61,11 @@ public class Group : MonoBehaviour
     {
         if (!_drivenByPlayer)
         {
-
+            if (_real.transform.childCount == 0)
+            {
+                Destroy(_real.gameObject);
+                Destroy(gameObject);
+            }
         }
         else
         {
@@ -79,19 +86,43 @@ public class Group : MonoBehaviour
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                _real.transform.rotation *= Quaternion.Euler(Vector3.forward * 90);
+                var euler = Vector3.forward * 90;
+
+                _real.transform.rotation *= Quaternion.Euler(euler);
+                if (IsValidGridPos())
+                    UpdateGrid();
+                else
+                    _real.transform.rotation *= Quaternion.Euler(-euler);
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                _real.transform.rotation *= Quaternion.Euler(Vector3.forward * -90);
+                var euler = Vector3.forward * -90;
+
+                _real.transform.rotation *= Quaternion.Euler(euler);
+                if (IsValidGridPos())
+                    UpdateGrid();
+                else
+                    _real.transform.rotation *= Quaternion.Euler(-euler);
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                _real.transform.rotation *= Quaternion.Euler(Vector3.right * -90);
+                var euler = Vector3.right * -90;
+
+                _real.transform.rotation *= Quaternion.Euler(euler);
+                if (IsValidGridPos())
+                    UpdateGrid();
+                else
+                    _real.transform.rotation *= Quaternion.Euler(-euler);
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                _real.transform.rotation *= Quaternion.Euler(Vector3.right * 90);
+                var euler = Vector3.right * 90;
+
+                _real.transform.rotation *= Quaternion.Euler(euler);
+                if (IsValidGridPos())
+                    UpdateGrid();
+                else
+                    _real.transform.rotation *= Quaternion.Euler(-euler);
             }
         }
 
